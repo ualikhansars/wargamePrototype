33c07aced353ec50c55147ccc368b6963c87ff4f
ualikhansars <valikhan754@gmail.com>
2017-09-12 02:23:02 -0700

    Create createObstacles function

--- src/map/createMap.ts
+++ src/map/createMap.ts
@@ -47,7 +47,7 @@ export const neighbours = (node:any) => {
       y: node.y + dir.y,
       distance: dir.distance
     }
-    if(neighbour.x >= 0 && neighbour.x < 1000 && neighbour.y >= 0 && neighbour.y < 600) {
+    if(neighbour.x >= 0 && neighbour.x < WIDTH && neighbour.y >= 0 && neighbour.y < HEIGHT) {
         let finded:boolean = false;
         for(let node of map) {
           if(neighbour.x === node.x && neighbour.y === node.y) {
@@ -73,16 +73,46 @@ export const addNeighbours = (map:any[]) => {
   }
 }
 
-export const createObstacles = (positionX:number, positionY:number) => {
+export const createOneObstacle = (positionX:number, positionY:number, type:string='forest') => {
   let node = {
     x: positionX,
     y: positionY
   };
-  ctx.fillRect(node.x, node.y, gridSize, gridSize);
+  if(type === 'forest') ctx.fillStyle = 'green';
+  else if(type === 'mountain') ctx.fillStyle = '#8B4513';
+  else if(type === 'river') ctx.fillStyle = 'blue';
+  ctx.fillRect(positionX, positionY, gridSize, gridSize);
   return deleteObjectFromArray(node, map)
 }
 
+export const createObstacles = (startX:number, finishX:number, startY:number, finishY:number, type:string='forest') => {
+  let newMap:any[] = map;
+  for(let x = startX; x <= finishX; x += gridSize) {
+    for(let y = startY; y <= finishY; y += gridSize) {
+      let node = {
+        x,
+        y
+      }
+      newMap = deleteObjectFromArray(node, newMap);
+      if(type === 'forest') ctx.fillStyle = 'green';
+      else if(type === 'mountain') ctx.fillStyle = '#8B4513';
+      else if(type === 'river') ctx.fillStyle = 'blue';
+      let xLength = Math.abs(startX - finishX);
+      let yLength = Math.abs(startY - finishY);
+      ctx.fillRect(x, y, gridSize, gridSize);
+    }
+  }
+  console.log('newMap', newMap);
+  return newMap;
+}
+
 export let map = createNodes();
-map = createObstacles(500, 100);
-map = createObstacles(500, 200);
+map = createObstacles(120, 160, 120, 160, 'river');
+map = createObstacles(660, 820, 180, 200, 'river');
+map = createObstacles(900, 1180, 180, 200, 'river');
+map = createOneObstacle(300, 340, 'mountain');
+map = createObstacles(280, 320, 360, 380, 'mountain');
+map = createObstacles(740, 760, 420, 500, 'forest');
+map = createObstacles(960, 1000, 440, 460, 'forest');
+map = createObstacles(980, 1000, 440, 520, 'forest');
 addNeighbours(map);
--- src/map/mapConfig.ts
+++ src/map/mapConfig.ts
@@ -1,7 +1,7 @@
 // global variables
-export const WIDTH: number = 1000;
+export const WIDTH: number = 1200;
 export const HEIGHT: number = 600;
-export const gridSize:number = 100;
+export const gridSize:number = 20;
 
 // create Canvas
 export let canvas = document.createElement('canvas');
